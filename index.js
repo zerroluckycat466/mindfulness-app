59f8544917ba54b8b09586a14c19d4490f285295
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-04 01:13:33 +0800

    Fix greeting message in the homepage route

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Willkommen zur Mindfulness App!');
+  res.send('Welcome to the Mindfulness App!');
 });
 
 app.listen(port, () => {
-  console.log(`Server läuft auf http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
