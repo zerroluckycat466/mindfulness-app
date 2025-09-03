06f9c69c01c4ecbfa41ea0baa096ede3334a9289
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-04 01:16:49 +0800

    Fix formatting and update response message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Mindfulness App!');
+  res.send('Welcome to the Mindfulness Application!');
 });
 
 app.listen(port, () => {
